176fce918c35b9a1c664320a49bb9587227811e4
1360151219 <1360151219@qq.com>
2022-11-10 22:53:55 +0800

    feat: parse scss

--- server/src/helper.ts
+++ server/src/helper.ts
@@ -2,6 +2,7 @@ import { transformFromAstSync } from '@babel/core';
 import { parse } from '@babel/parser';
 import parseJsx from './plugins/parseJsx';
 import { IClassName } from './types';
+
 export function transformClassName(code: string, file: string) {
   const ast = parse(code, {
     sourceType: 'unambiguous',
@@ -11,13 +12,15 @@ export function transformClassName(code: string, file: string) {
   transformFromAstSync(ast, code, {
     plugins: [[parseJsx, { classname, file }]],
   });
-  console.log(classname);
+  console.log('parse tsx', classname);
+
   return classname;
 }
 
 export function removeQuota(str: string) {
   return /(["'])[\d\w]+\1/.test(str) ? str.slice(1, str.length - 1) : str;
 }
+
 export function removeTemplateLiteral(str: string) {
   const m = /\{`([-_\d\w]+)`\}/.exec(str); //'{`foo3`}' {`foo_foo-4`}
   // console.log(m, str);
@@ -27,6 +30,7 @@ export function removeTemplateLiteral(str: string) {
     return str;
   }
 }
+
 export function createRange(m: RegExpMatchArray) {
   const start = m.index || 0;
   const end = start + m[0].length;
@@ -35,6 +39,7 @@ export function createRange(m: RegExpMatchArray) {
     end,
   };
 }
+
 export function getDefinationClass(text: string, character: number) {
   const wordReg = /\.([-_\d\w]+)/g;
   const m = text.matchAll(wordReg) || [];
